b9846992aaaf075cceb2e5e4592d9c3d2bb5e6f6
uriel2012 <134099307+uriel2012@users.noreply.github.com>
2023-08-14 18:02:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+// Crea una referencia para el elemento canvas
+canvas=document.getElementById("mycanvas");
+ctx=canvas.getContext("2d");
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //define una variable para una imagen nueva
+	img_imgTag.onload = uploadimg; // establece una función para cargar la variable
+	img_imgTag.src = img_image;   // carga la imagen
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Escribe el código para obtener el evento key-pressed
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+			aplhabetkey();
+			document.getElementById("d1").innerHTML="presionaste una letra";
+		}
+	else {
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+	img_image="Alpkey.png"
+	add();
+
+}
+function numberkey()
+{
+	
+}
+function arrowkey()
+{
+}
+function specialkey()
+{
+	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
